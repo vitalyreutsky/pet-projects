3d4e4b3029604ad751cd3352459dd599685e2055
Vitaly Reutsky <vreutsky77@mail.ru>
2021-12-11 21:05:31 +0300

    added project incrementing counter

---
+++ incrementing counter/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.textContent = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 2);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
