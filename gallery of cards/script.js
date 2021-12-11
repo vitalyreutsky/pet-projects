a5c487caf475329ccb942f141ef3293e81d263ca
Vitaly Reutsky <vreutsky77@mail.ru>
2021-12-11 18:53:05 +0300

    added project gallery of cards

---
+++ gallery of cards/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const slides = document.querySelectorAll(".slide");
+
+for (let slide of slides) {
+  slide.addEventListener("click", () => {
+    clearActiveClasses();
+
+    slide.classList.add("active");
+  });
+}
+
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+}
