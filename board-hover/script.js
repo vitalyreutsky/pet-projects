4c6061c982b8c7ecd695e587e64266d480dd0891
Vitaly Reutsky <vreutsky77@mail.ru>
2021-12-12 19:20:07 +0300

    added project board-hover

---
+++ board-hover/script.js
@@ -0,0 +1,30 @@
+const board = document.getElementById("board");
+const colors = ["red", "blue", "yellow", "purple"];
+const squaresNumber = 500;
+
+for (let i = 0; i < squaresNumber; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
